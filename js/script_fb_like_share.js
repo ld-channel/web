9ef84418d8dfd367a567ebde413b0f843af27a97
ld-channel <36303746+ld-channel@users.noreply.github.com>
2018-04-01 22:34:18 +0700

    updated

---
+++ js/script_fb_like_share.js
@@ -0,0 +1,2 @@
+<iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fweb.facebook.com%2FLD.Khmer.Karaoke%2F&width=450&layout=standard&action=like&size=small&show_faces=true&share=true&height=80&appId=211355292749536"
+    width="450" height="80" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
