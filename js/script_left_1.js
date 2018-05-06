daccad18ecdacd73f376f086c1bdb15cdf10aa6d
ld-channel <36303746+ld-channel@users.noreply.github.com>
2018-05-06 14:55:31 +0700

    updated

--- js/script_left_1.js
+++ js/script_left_1.js
@@ -1,3 +1,3 @@
 
-var url = "https://ld-channel.github.io/web/ads/rob.html";
+var url = "https://ld-channel.github.io/web/ads/cream-for-acnes.html";
 document.write('<a href="' + url + '" target="_blank"><div class="gallery_ads1"><div class="desc_ads1"></div><div id="ads_left"></div></div></a>');
